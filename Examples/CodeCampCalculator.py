b6d98b54d23bc4ad427427909fef2314e43b7b80
MattVeloso <45905939+Mattveloso@users.noreply.github.com>
2020-05-04 16:51:32 -0300

    I've added more examples and changed manual polygon creation to a loop

---
+++ Examples/CodeCampCalculator.py
@@ -0,0 +1,62 @@
+from tkinter import *
+
+root = Tk()
+root.title('Simple Calculator')
+
+e = Entry(root, width=35, borderwidth=5)
+e.grid(row=0,column=0, columnspan=3,padx=10,pady=10)
+
+def button_click(number):
+	current = e.get()
+	e.delete(0,END)
+	e.insert(0,str(current)+str(number))
+
+def button_clear():
+	e.delete(0,END)
+
+def button_add():
+	first_number=e.get()
+	global f_num
+	f_num=int(first_number)
+	e.delete(0,END)
+
+def button_equal():
+	second_number=e.get()
+	e.delete(0,END)
+	e.insert(0,f_num+int(second_number))
+
+#buttons
+but_1= Button(root, text="1",padx=40,pady=20,command=lambda: button_click(1))
+but_2= Button(root, text="2",padx=40,pady=20,command=lambda: button_click(2))
+but_3= Button(root, text="3",padx=40,pady=20,command=lambda: button_click(3))
+but_4= Button(root, text="4",padx=40,pady=20,command=lambda: button_click(4))
+but_5= Button(root, text="5",padx=40,pady=20,command=lambda: button_click(5))
+but_6= Button(root, text="6",padx=40,pady=20,command=lambda: button_click(6))
+but_7= Button(root, text="7",padx=40,pady=20,command=lambda: button_click(7))
+but_8= Button(root, text="8",padx=40,pady=20,command=lambda: button_click(8))
+but_9= Button(root, text="9",padx=40,pady=20,command=lambda: button_click(9))
+but_0= Button(root, text="0",padx=40,pady=20,command=lambda: button_click(0))
+but_add= Button(root, text="+",padx=39,pady=20,command=button_add)
+but_equal= Button(root, text="=",padx=85,pady=20,command=button_equal)
+but_clear= Button(root, text="Clc",padx=80,pady=20,command= button_clear)
+
+#put the buttons on the screen
+but_1.grid(row=3,column=0)
+but_2.grid(row=3,column=1)
+but_3.grid(row=3,column=2)
+
+but_4.grid(row=2,column=0)
+but_5.grid(row=2,column=1)
+but_6.grid(row=2,column=2)
+
+but_7.grid(row=1,column=0)
+but_8.grid(row=1,column=1)
+but_9.grid(row=1,column=2)
+
+but_0.grid(row=4,column=0)
+
+but_clear.grid(row=4, column=1,columnspan=2)
+but_add.grid(row=5,column=0)
+but_equal.grid(row=5,column=1,columnspan=2)
+
+root.mainloop()
--- NewGUI.py
+++ NewGUI.py
@@ -1,1 +1,36 @@
-import tkinter as tk
+
+
+
+#M
+def cheapPattern():
+    Cheap_Canvas = Canvas(Canvas_Frame, width=300, height=300)
+    Cheap_Canvas.grid(row=0, column=0, columnspan=5, rowspan=5)
+
+    #creates column coordenates for placement of polygons to create CHEAP PATTERN
+    col1=[0,75,150,225]*10 #reoeats every 4
+    col2=range(30,301,30) #Creates tuple with numbers from 30 to 300, changes every 4
+    col2_altered =[]
+    for value in col2: #for loop for creating a list that only changes values every 4
+        for repetition in range(0,4):
+            col2_altered.append(value)
+    col2=col2_altered
+    print(col2)
+    col3=[75,150,225,300]*10 #repeats every 4
+    col4=col2 #they're the same, so let's keep it equal
+    col5=[55,130,205,280]*10 #repeats every 4
+    col6=range(0,271,30) #changes every 4
+    col6_altered =[]
+    for value in col6: #for loop for creating a list that only changes values every 4
+        for repetition in range(0,4): #Do this 4x before going forward
+            col6_altered.append(value)
+    col6=col6_altered
+    print(col6)
+    col7=[20,95,170,245]*10 #repeats every 4
+    col8=col6
+
+    for number in range(0,40): #as we need polygons, let's create the polygons with a for loop
+        Cheap_Canvas.create_polygon(col1[number],col2[number],col3[number],col4[number],
+                                    col5[number],col6[number],col7[number],col8[number],
+                                    fill=colourFill.get(),outline="black")
+    ColorCReset()
+#\M
